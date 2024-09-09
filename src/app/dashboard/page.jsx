6ffc43e546ab64d86dcb9297192cc8c1dba516d4
Shehan Fernando <shehannavindu30@gmail.com>
2024-09-09 16:36:56 +0530

    Dashboard update

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <hr className='w-3/4 mx-auto my-0' />
+      <div className='max-w-5xl mx-auto p-5 space-y-7'>
+        <h1 className='text-2xl font-medium text-indigo-700'>Pensala Suggestions</h1>
+        <p>We are diligently working to enhance our suggestion capabilities to 
+          provide you with more effective and tailored recommendations promptly. 
+          Your satisfaction is our utmost priority, and we appreciate your patience 
+          as we strive to deliver the best possible experience.
+        </p>
+      </div>
+    </div>
+  )
+}
+  
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+       <hr className='w-3/4 mx-auto my-0' />
+
+    </div>
   )
 }
