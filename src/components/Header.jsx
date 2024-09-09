59d633eeec0a1648e54e96b3558ecfe6d8ea6522
Shehan Fernando <shehannavindu30@gmail.com>
2024-09-09 12:58:21 +0530

    Update the pensala logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import MenuItem from './MenuItem';
 import { PiPencilCircleFill } from "react-icons/pi";
-import { RiDashboard3Line } from "react-icons/ri";
+import { VscDashboard } from "react-icons/vsc";
+import Link from 'next/link';
 
 
 export default function Header() {
   return (
-    <div>
-        <div className=''>
+    <div className='flex justify-between items-center p-5 max-w-4xl mx-auto'>
+        <div className='flex gap-4'>
             <MenuItem title="Home" address="/home" Icon={PiPencilCircleFill}/>
-            <MenuItem title="Dashboard" address="/dashboard" Icon={RiDashboard3Line}/>
-
-
+            <MenuItem title="Dashboard" address="/dashboard" Icon={VscDashboard}/>
         </div>
-        <div className=''>
-
-        </div>
+        <Link href={'/home'} className='flex items-center'>
+          <span className= 'text-white font-bold bg-indigo-700 py-1 px-2 rounded-lg'>PENSALA</span>
+          <span className='hidden sm:inline text-indigo-700 font-bold'>.COM</span>
+        </Link>
     </div>
   )
 }
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function MenuItem({title,address,Icon}) {
   return (
-    <Link href={address}>
-        <Icon/>
+    <Link href={address} className='hover:text-indigo-700'>
+        <Icon className='text-4xl sm:hidden'/>
         <p className='uppercase hidden sm:inline'>{title}</p>
     </Link>
 
